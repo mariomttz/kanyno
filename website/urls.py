c33e7cc7664b2dff33959f7ca7c354192037f9c3
Mario Martinez <mariomttz@comunidad.unam.mx>
2023-05-15 19:21:21 -0600

    URLs y vistas actualizadas

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,24 @@
+# Libraries and packages
 from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # Website URLs
+    path('',            home,       name = 'home'),
+    path('calculator/', calculator, name = 'calculator'),
+    path('about/',      about,      name = 'about'),
+    path('contact/',    contact,    name = 'contact'),
+    path('search/',     search,     name = 'search'),
+    path('signup/',     signup,     name = 'signup'),
+    path('login/',      login,      name = 'login'),
+    path('logout/',     logout,     name = 'logout'),
+
+    # Users and pets URLs
+    path('account/',        viewUserAccount, name = 'account'),
+    path('account/edit/',   editUserAccount, name = 'edit-account'),
+    path('account/delete/', delUserAccount,  name = 'del-account'),
+    path('pets/',           viewPets,        name = 'pets'),
+    path('pets/add/',       addPet,          name = 'add-pet'),
+    path('pets/edit/',      editPet,         name = 'edit-pet'),
+    path('pets/delete/',    delPet,          name = 'del-pet'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,65 @@
+# Libraries and packages 
 from django.shortcuts import render
 
-def login(request):
+# Website views
+def home(request):
+    return render(request, 'home.html')
+
+def calculator(request):
+    if request.method == 'POST':
+        pass
+
+    else:
+        return render(request, 'calculator.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        pass
+
+    else:
+        return render(request, 'contact.html')
+
+def search(request):
     pass
 
 def signup(request):
+    if request.method == 'POST':
+        pass
+
+    else:
+        return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        pass
+
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
     pass
 
+# Users and pets views
+def viewUserAccount(request):
+    return render(request, 'userAccount.html')
+
+def editUserAccount(request):
+    pass
+
+def delUserAccount(request):
+    pass
+
+def viewPets(request):
+    return render(request, 'pets.html')
+
+def addPet(request):
+    pass
+
+def editPet(request):
+    pass
+
+def delPet(request):
+    pass
